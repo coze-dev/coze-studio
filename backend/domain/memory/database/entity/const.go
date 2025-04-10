63707dfaff546b9a4c9d030006ecdcaf48790b53
liujian.0502 <liujian.0502@bytedance.com>
2025-04-10 16:03:10 +0800

    feat: define database domain

---
+++ backend/domain/memory/database/entity/const.go
@@ -0,0 +1,76 @@
+package entity
+
+type TableStatus int64
+
+const (
+	TableStatus_Online TableStatus = 1
+	TableStatus_Delete TableStatus = 2
+)
+
+type DatabaseRWMode int64
+
+const (
+	BotTableRWMode_LimitedReadWrite   DatabaseRWMode = 1
+	BotTableRWMode_ReadOnly           DatabaseRWMode = 2
+	BotTableRWMode_UnlimitedReadWrite DatabaseRWMode = 3
+	BotTableRWMode_RWModeMax          DatabaseRWMode = 4
+)
+
+type FieldItemType int64
+
+const (
+	FieldItemType_Text     FieldItemType = 1
+	FieldItemType_Number   FieldItemType = 2
+	FieldItemType_Date     FieldItemType = 3
+	FieldItemType_Float    FieldItemType = 4
+	FieldItemType_Boolean  FieldItemType = 5
+	FieldItemType_FullText FieldItemType = 6
+)
+
+type TableType int64
+
+const (
+	TableType_DraftTable  TableType = 1
+	TableType_OnlineTable TableType = 2
+)
+
+type OperateType int64
+
+const (
+	OperateType_Custom OperateType = 0
+	OperateType_Insert OperateType = 1
+	OperateType_Update OperateType = 2
+	OperateType_Delete OperateType = 3
+	OperateType_Select OperateType = 4
+)
+
+type SortDirection int64
+
+const (
+	SortDirection_ASC  SortDirection = 1
+	SortDirection_Desc SortDirection = 2
+)
+
+type Operation int64
+
+const (
+	Operation_EQUAL         Operation = 1
+	Operation_NOT_EQUAL     Operation = 2
+	Operation_GREATER_THAN  Operation = 3
+	Operation_LESS_THAN     Operation = 4
+	Operation_GREATER_EQUAL Operation = 5
+	Operation_LESS_EQUAL    Operation = 6
+	Operation_IN            Operation = 7
+	Operation_NOT_IN        Operation = 8
+	Operation_IS_NULL       Operation = 9
+	Operation_IS_NOT_NULL   Operation = 10
+	Operation_LIKE          Operation = 11
+	Operation_NOT_LIKE      Operation = 12
+)
+
+type Logic int64
+
+const (
+	Logic_And Logic = 1
+	Logic_Or  Logic = 2
+)
---
+++ backend/domain/memory/database/interface.go
@@ -0,0 +1,143 @@
+package database
+
+import (
+	"context"
+
+	"code.byted.org/flow/opencoze/backend/domain/memory/database/entity"
+	userEntity "code.byted.org/flow/opencoze/backend/domain/user/entity"
+)
+
+type Database interface {
+	CreatDatabase(ctx context.Context, req *CreatDatabaseRequest) (*CreatDatabaseResponse, error)
+	UpdateDatabase(ctx context.Context, req *UpdateDatabaseRequest) error
+	DeleteDatabase(ctx context.Context, req *DeleteDatabaseRequest) error
+	MGetDatabase(ctx context.Context, ids []int64) (*MGetDatabaseResponse, error)
+	ListDatabase(ctx context.Context, req *ListDatabaseRequest) (*ListDatabaseResponse, error)
+
+	AddDatabaseRecord(ctx context.Context, req *AddDatabaseRecordRequest) error
+	UpdateDatabaseRecord(ctx context.Context, req *UpdateDatabaseRecordRequest) error
+	DeleteDatabaseRecord(ctx context.Context, req *DeleteDatabaseRecordRequest) error
+	ListDatabaseRecord(ctx context.Context, req *ListDatabaseRecordRequest) (*ListDatabaseRecordResponse, error)
+
+	ExecuteSQL(ctx context.Context, req *ExecuteSQLRequest) (*ExecuteSQLResponse, error)
+}
+
+type CreatDatabaseRequest struct {
+	Database *entity.Database
+}
+
+type CreatDatabaseResponse struct {
+	Database *entity.Database
+}
+
+type UpdateDatabaseRequest struct {
+	Database *entity.Database
+}
+
+type DeleteDatabaseRequest struct {
+	Database *entity.Database
+}
+
+type MGetDatabaseResponse struct {
+	Database []*entity.Database
+}
+
+type ListDatabaseRequest struct {
+	CreatorID   *int64
+	SpaceID     *int64
+	ConnectorID *int64
+	TableName   *string
+	TableType   entity.TableType
+
+	Limit  int
+	Cursor *string
+}
+
+type ListDatabaseResponse struct {
+	Databases []*entity.Database
+
+	HasMore    bool
+	NextCursor *string
+}
+
+type AddDatabaseRecordRequest struct {
+	DatabaseID  int64
+	TableType   entity.TableType
+	ConnectorID *int64
+	Records     []map[string]string
+}
+
+type UpdateDatabaseRecordRequest struct {
+	DatabaseID  int64
+	TableType   entity.TableType
+	ConnectorID *int64
+	Records     []map[string]string
+}
+
+type DeleteDatabaseRecordRequest struct {
+	DatabaseID  int64
+	TableType   entity.TableType
+	ConnectorID *int64
+	Records     []map[string]string
+}
+
+type ListDatabaseRecordRequest struct {
+	DatabaseID  int64
+	ConnectorID *int64
+	TableType   entity.TableType
+
+	Limit  int
+	Cursor *string
+}
+
+type ListDatabaseRecordResponse struct {
+	Records   []map[string]string
+	FieldList []*entity.FieldItem
+
+	HasMore    bool
+	NextCursor *string
+}
+
+type SelectFieldList struct {
+	FieldID    []string
+	IsDistinct bool
+}
+
+type OrderBy struct {
+	Field     string
+	Direction entity.SortDirection
+}
+
+type ComplexCondition struct {
+	Conditions       []*Condition
+	NestedConditions *ComplexCondition
+	Logic            entity.Logic
+}
+
+type Condition struct {
+	Left      string
+	Operation entity.Operation
+	Right     string
+}
+
+type ExecuteSQLRequest struct {
+	SQL         *string // set if OperateType is 0.
+	DatabaseID  int64
+	User        *userEntity.UserIdentity
+	ConnectorID *int64
+	SQLParams   []*entity.SQLParamVal
+	TableType   entity.TableType
+	OperateType entity.OperateType
+
+	// set the following values if OperateType is not 0.
+	SelectFieldList *SelectFieldList
+	OrderByList     []OrderBy
+	Limit           *int64
+	Offset          *int64
+	Condition       *ComplexCondition
+}
+
+type ExecuteSQLResponse struct {
+	Records   []map[string]string
+	FieldList []*entity.FieldItem
+}
