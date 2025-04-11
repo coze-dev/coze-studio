e2bf0be9d9a73357bb69eb8248185b0c86cba5dd
liujian.0502 <liujian.0502@bytedance.com>
2025-04-11 15:57:45 +0800

    feat: add sql operate in infra

---
+++ backend/domain/memory/infra/mysql/entity/sql.go
@@ -0,0 +1,38 @@
+package entity
+
+type Column struct {
+	Name          string
+	DataType      DataType
+	Length        *int
+	NotNull       bool
+	DefaultValue  *string
+	AutoIncrement bool // 表示该列是否为自动递增
+	Comment       *string
+}
+
+type Index struct {
+	Name    string
+	Type    IndexType
+	Columns []string
+}
+
+type TableOption struct {
+	Collate       *string
+	AutoIncrement *int64 // 设置表的自动递增初始值
+	Comment       *string
+}
+
+type Table struct {
+	Name      string // 保证唯一性
+	Columns   []*Column
+	Indexes   []*Index
+	Options   *TableOption
+	CreatedAt int64
+	UpdatedAt int64
+}
+
+type ResultSet struct {
+	Columns      []string
+	Rows         []map[string]interface{}
+	AffectedRows int64
+}
