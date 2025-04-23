b6f3672be1aa17e5fe854b2859320f80c2b21640
fanlv <fanlv@bytedance.com>
2025-04-23 17:39:07 +0800

    feat(backend):add storage interface and minio implement
    Change-Id: Id338d4d67239c6dbda349d7ae0670cc96d7e284d

--- backend/types/consts/consts.go
+++ backend/types/consts/consts.go
@@ -8,4 +8,8 @@ const (
 	VeImageXServerID = "VE_IMAGEX_SERVER_ID"
 	VeImageXDomain   = "VE_IMAGEX_DOMAIN"
 	VeImageXTemplate = "VE_IMAGEX_TEMPLATE"
+
+	MinIO_AK       = "MINIO_AK"
+	MinIO_SK       = "MINIO_SK"
+	MinIO_Endpoint = "MINIO_ENDPOINT"
 )
