bfed57f17ab2c1f54a71464d963e2f7a35e952af
刘运超 <liuyunchao.0510@bytedance.com>
2025-05-08 17:33:45 +0800

    修改api定义

--- idl/data_engine/dataset/dataset.thrift
+++ idl/data_engine/dataset/dataset.thrift
@@ -7,18 +7,18 @@ namespace go flow.dataengine.dataset
 struct CreateDatasetRequest  {
     1: string name                   // 知识库名称，长度不超过100个字符
     2: string description
-    3: i64 space_id (agw.js_conv="str")
+    3: i64 space_id (api.js_conv="str")
     4: string icon_uri
     5: common.FormatType format_type
-    6: i64 biz_id (agw.js_conv="str") // 开放给第三方的业务标识, coze 传 0 或者不传
-    7: i64 project_id (agw.js_conv="str") //新增project ID
+    6: i64 biz_id (api.js_conv="str") // 开放给第三方的业务标识, coze 传 0 或者不传
+    7: i64 project_id (api.js_conv="str") //新增project ID
 
     255: optional base.Base Base
 }
 
 
 struct CreateDatasetResponse {
-    1: i64 dataset_id (agw.js_conv="str")
+    1: i64 dataset_id (api.js_conv="str")
 
     253: required i64 code
     254: required string msg
@@ -26,15 +26,15 @@ struct CreateDatasetResponse {
 }
 
 struct DatasetDetailRequest {
-    1: list<i64>  dataset_ids (agw.js_conv="str")
-    3: i64 project_id (agw.js_conv="str") //新增project ID
-    2: i64 space_id (agw.js_conv="str")
+    1: list<i64>  dataset_ids (api.js_conv="str")
+    3: i64 project_id (api.js_conv="str") //新增project ID
+    2: i64 space_id (api.js_conv="str")
 
     255: optional base.Base Base
 }
 
 struct DatasetDetailResponse {
-    1: map<i64, Dataset>     dataset_details (agw.js_conv="str")
+    1: map<i64, Dataset>     dataset_details (api.js_conv="str")
 
     253: required i64 code
     254: required string msg
@@ -51,7 +51,7 @@ enum DatasetStatus {
 
 
 struct Dataset {
-    1:  i64 dataset_id(agw.js_conv="str", api.js_conv="true")
+    1:  i64 dataset_id(api.js_conv="str", api.js_conv="true")
     2:  string        name                 // 数据集名称
     3:  list<string>  file_list            // 文件列表
     4:  i64           all_file_size (api.js_conv="str")  // 所有文件大小
@@ -64,8 +64,8 @@ struct Dataset {
     11: string        icon_uri
     12: bool          can_edit             // 是否可以编辑
     13: i32           create_time          // 创建时间，秒级时间戳
-    14: i64           creator_id(agw.js_conv="str", api.js_conv="true")  // 创建者ID
-    15: i64           space_id(agw.js_conv="str", api.js_conv="true")   // 空间ID
+    14: i64           creator_id(api.js_conv="str", api.js_conv="true")  // 创建者ID
+    15: i64           space_id(api.js_conv="str", api.js_conv="true")   // 空间ID
     18: list<string>  failed_file_list  // 处理失败的文件
 
     19: common.FormatType  format_type
@@ -74,7 +74,7 @@ struct Dataset {
     22: i32                doc_count          // 文档数量
     23: common.ChunkStrategy  chunk_strategy  // 切片规则
 
-    24: list<i64>     processing_file_id_list(agw.js_conv="str", api.js_conv="true")  // 处理中的文件ID列表
+    24: list<i64>     processing_file_id_list(api.js_conv="str", api.js_conv="true")  // 处理中的文件ID列表
     25: string        project_id          //新增project ID
 }
 
@@ -83,11 +83,11 @@ struct ListDatasetRequest {
 
     3: optional i32 page
     4: optional i32 size
-    5: i64 space_id (agw.js_conv="str")
+    5: i64 space_id (api.js_conv="str")
     6: optional common.OrderField  order_field  // 排序字段
     7: optional common.OrderType   order_type   // 排序规则
     8: optional string space_auth // 如果传了指定值, 就放开校验
-    9: optional i64 biz_id (agw.js_conv="str") // 开放给第三方的业务标识
+    9: optional i64 biz_id (api.js_conv="str") // 开放给第三方的业务标识
     10: optional bool need_ref_bots // 是否需要拉取引用bots的数量，会增加响应延时
     11: optional string project_id //新增project ID
     255: optional base.Base Base
@@ -103,7 +103,7 @@ struct ListDatasetResponse {
 struct DatasetFilter {
     // 如果都设置了，And 关系
     1: optional string name              // 关键字搜索, 按照名称模糊匹配
-    2: optional list<i64>  dataset_ids (agw.js_conv="str") // deprecated
+    2: optional list<i64>  dataset_ids (api.js_conv="str") // deprecated
     3: optional DatasetSource source_type   // 来源
     4: optional DatasetScopeType  scope_type   // 搜索类型
     5: optional common.FormatType format_type // 类型
@@ -120,7 +120,7 @@ enum DatasetSource{
 }
 
 struct DeleteDatasetRequest {
-    1: i64 dataset_id (agw.js_conv="str")
+    1: i64 dataset_id (api.js_conv="str")
 
     255: optional base.Base Base
 }
@@ -133,7 +133,7 @@ struct DeleteDatasetResponse {
 }
 
 struct UpdateDatasetRequest {
-    1: i64                 dataset_id (agw.js_conv="str")
+    1: i64                 dataset_id (api.js_conv="str")
     2: string              name
     3: string              icon_uri
     4: string              description
