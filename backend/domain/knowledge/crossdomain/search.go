6d43859a55d92ab5f656b340d6fa764708c5db58
刘运超 <liuyunchao.0510@bytedance.com>
2025-05-09 16:45:36 +0800

    case修复

--- backend/domain/knowledge/crossdomain/search.go
+++ backend/domain/knowledge/crossdomain/search.go
@@ -6,6 +6,7 @@ import (
 	"code.byted.org/flow/opencoze/backend/domain/search/entity"
 )
 
+//go:generate  mockgen -destination ../../../internal/mock/domain/knowledge/search_mock.go --package mock -source search.go
 type DomainNotifier interface {
 	PublishResources(ctx context.Context, event *entity.ResourceDomainEvent) error
 }
