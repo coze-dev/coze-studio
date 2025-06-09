1a47066d0f2ffebeabd01175a1e6efca38d71d4b
liuyunchao.0510 <liuyunchao.0510@bytedance.com>
2025-06-09 20:28:27 +0800

    extra

--- backend/domain/knowledge/internal/consts/consts.go
+++ backend/domain/knowledge/internal/consts/consts.go
@@ -3,9 +3,6 @@ package consts
 const (
 	// RDBFieldID primary key id
 	RDBFieldID    = "_knowledge_document_slice_id"
-	KnowledgeID   = "knowledge_id"
 	KnowledgeName = "knowledge_name"
-	DocumentID    = "document_id"
-	DocumentName  = "document_name"
 	DocumentURL   = "document_url"
 )
