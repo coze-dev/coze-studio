74065b37ab93c6b90fb21475ca77636004963525
liuyunchao.0510 <liuyunchao.0510@bytedance.com>
2025-06-10 11:03:20 +0800

    暂存

--- backend/types/errno/knowledge.go
+++ backend/types/errno/knowledge.go
@@ -4,9 +4,25 @@ import "code.byted.org/flow/opencoze/backend/pkg/errorx/code"
 
 // Knowledge: 105 000 000 ~ 105 999 999
 const (
-	ErrKnowledgeInvalidParamCode = 105000000
-	ErrKnowledgePermissionCode   = 105000001
-	ErrKnowledgeNonRetryableCode = 105000002
+	ErrKnowledgeInvalidParamCode             = 105000000
+	ErrKnowledgePermissionCode               = 105000001
+	ErrKnowledgeNonRetryableCode             = 105000002
+	ErrKnowledgeDBCode                       = 105000003
+	ErrKnowledgeSearchStoreCode              = 105000004
+	ErrKnowledgeSystemCode                   = 105000005
+	ErrKnowledgeCrossDomainCode              = 105000006
+	ErrKnowledgeEmbeddingCode                = 105000007
+	ErrKnowledgeIDGenCode                    = 105000008
+	ErrKnowledgeMQCode                       = 105000009
+	ErrKnowledgeDuplicateCode                = 105000010
+	ErrKnowledgeNotExistCode                 = 105000011
+	ErrKnowledgeDocumentNotExistCode         = 105000012
+	ErrKnowledgeSemanticColumnValueEmptyCode = 105000013
+	ErrKnowledgeParseJSONCode                = 105000014
+	ErrKnowledgeResegmentNotSupportedCode    = 105000015
+	ErrKnowledgeFieldNameDuplicatedCode      = 105000016
+	ErrKnowledgeDocOversizeCode              = 105000017
+	
 )
 
 func init() {
