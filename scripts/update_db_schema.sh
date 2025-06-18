3f79de1ab5ed6360a87224ffca134e9d3bef8e0b
fanlv <fanlv@bytedance.com>
2025-06-18 15:56:49 +0800

    feat:(backend): add update_db_schema.sh

--- scripts/update_db_schema.sh
+++ scripts/update_db_schema.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DOCKER_DIR="$(cd "$SCRIPT_DIR/../../docker" && pwd)"
-BACKEND_DIR="$(cd "$SCRIPT_DIR/../../backend" && pwd)"
+BASE_DIR="$(dirname "$SCRIPT_DIR")"
+ATLAS_DIR="$BASE_DIR/docker/atlas"
 
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-cd "$DOCKER_DIR/atlas"
-
-source "$BACKEND_DIR/.env"
-echo "ATLAS_URL: $ATLAS_URL"
-
 # Check if ATLAS_URL is set
 if [ -z "$ATLAS_URL" ]; then
     echo -e "${RED}Error: ATLAS_URL is not set. Please set the ATLAS_URL environment variable.${NC}"
     exit 1
 fi
+
+echo "ATLAS_URL: $ATLAS_URL"
 
 #  check if atlas is installed
 OS=$(uname -s)
@@ -42,22 +39,8 @@ else
     fi
 fi
 
-cd "$DOCKER_DIR/atlas"
+cd $ATLAS_DIR
 
-atlas schema apply -u $ATLAS_URL --to file://opencoze_latest_schema.hcl --exclude "atlas_schema_revisions,table_*" --auto-approve
-echo -e "${GREEN}✅ apply mysql schema successfully${NC}"
+atlas migrate diff update --env local --to $ATLAS_URL
 
-# if [ "$OS" = "Darwin" ]; then
-#     atlas schema apply -u $ATLAS_URL --to file://opencoze_latest_schema.hcl --auto-approve --exclude "table_*"
-#     echo -e "${GREEN}✅ apply mysql schema successfully${NC}"
-# elif [ "$OS" = "Linux" ]; then
-#     atlas migrate apply \
-#         --url "$ATLAS_URL" \
-#         --dir "file://migrations" \
-#         --revisions-schema opencoze \
-#         --baseline "20250618025620"
-#     echo -e "${GREEN}✅ migrate mysql successfully${NC}"
-# elif [ "$OS" = "Windows" ]; then
-#     echo -e "${RED}Windows is not supported. Please install Atlas manually.${NC}"
-#     exit 1
-# fi
+atlas schema inspect -u $ATLAS_URL --exclude "atlas_schema_revisions,table_*" >opencoze_latest_schema.hcl
