742cd6395fd1289e44705d298325fc32932070d5
chenjiawei.inizio <chenjiawei.inizio@bytedance.com>
2025-06-18 16:47:50 +0800

    fix: rebuild fe.sh

--- scripts/build_fe.sh
+++ scripts/build_fe.sh
@@ -12,7 +12,7 @@ pushd "${FRONTEND_DIR}"
 
 echo "正在构建前端..."
 
-BUILD_BRANCH=opencoze-local rush build -o @coze-studio/app --verbose
+BUILD_BRANCH=opencoze-local rush rebuild -o @coze-studio/app --verbose
 
 popd
 
