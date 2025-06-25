23ab385b1526d5bc8f31cb49515a6e1096f0add0
lijunwen.gigoo <lijunwen.gigoo@bytedance.com>
2025-06-25 16:57:00 +0800

    feat: add doubao vision model

--- docker/volumes/mysql/sql_init.sql
+++ docker/volumes/mysql/sql_init.sql
@@ -27,13 +27,13 @@ ON DUPLICATE KEY UPDATE
 
 -- mock chat mode config for self-test, if publish should remove
 INSERT INTO `opencoze`.`model_meta` (`id`, `model_name`, `protocol`, `icon_uri`, `capability`, `conn_config`, `status`, `description`, `created_at`, `updated_at`, `deleted_at`)
-VALUES (123, '方舟', 'ark', '', '{\"support_sp\": true, \"function_call\": true, \"search_enhance\": false, \"support_stream\": true}', '{\"ark\": {\"region\": \"\", \"access_key\": \"bb5b06d3-06a1-42fb-9bbf-e7eef262e6a3\", \"secret_key\": \"\"}, \"model\": \"ep-20240923105216-clktf\", \"api_key\": \"bb5b06d3-06a1-42fb-9bbf-e7eef262e6a3\", \"base_url\": \"\"}', 1, '', 0, 0, NULL)
+VALUES (123, '方舟', 'ark', '', '{\"support_sp\": true, \"function_call\": true, \"search_enhance\": false, \"support_stream\": true}', '{\"ark\": {\"region\": \"\", \"access_key\": \"1ea0e64a-cef2-4f29-b96b-effaf97667ba\", \"secret_key\": \"\"}, \"model\": \"ep-20250623194625-rszwf\", \"api_key\": \"1ea0e64a-cef2-4f29-b96b-effaf97667ba\", \"base_url\": \"\"}', 1, '', 0, 0, NULL)
 ON DUPLICATE KEY UPDATE
    id = VALUES(id);
 
 
 INSERT INTO `opencoze`.`model_entity` (`id`, `meta_id`, `name`, `description`, `default_params`, `scenario`, `status`, `created_at`, `updated_at`, `deleted_at`)
-VALUES (1, 123, '豆包·1.5·Pro·32k', "", '[{\"name\":\"temperature\",\"label\":\"生成随机性\",\"desc\":\"- **temperature**: 调高温度会使得模型的输出更多样性和创新性，反之，降低温度会使输出内容更加遵循指令要求但减少多样性。建议不要与“Top p”同时调整。\",\"type\":\"float\",\"min\":\"0\",\"max\":\"1\",\"default_val\":{\"balance\":\"0.8\",\"creative\":\"1\",\"default_val\":\"1.0\",\"precise\":\"0.3\"},\"precision\":1,\"options\":null,\"param_class\":{\"class_id\":\"slider\",\"label\":\"生成随机性\"}},{\"name\":\"max_tokens\",\"label\":\"最大回复长度\",\"desc\":\"控制模型输出的Tokens 长度上限。通常 100 Tokens 约等于 150 个中文汉字。\",\"type\":\"int\",\"min\":\"1\",\"max\":\"12288\",\"default_val\":{\"default_val\":\"4096\"},\"options\":null,\"param_class\":{\"class_id\":\"slider\",\"label\":\"输入及输出设置\"}}]',
+VALUES (1, 123, '豆包·1.5·Pro·32k(废弃)', "", '[{\"name\":\"temperature\",\"label\":\"生成随机性\",\"desc\":\"- **temperature**: 调高温度会使得模型的输出更多样性和创新性，反之，降低温度会使输出内容更加遵循指令要求但减少多样性。建议不要与“Top p”同时调整。\",\"type\":\"float\",\"min\":\"0\",\"max\":\"1\",\"default_val\":{\"balance\":\"0.8\",\"creative\":\"1\",\"default_val\":\"1.0\",\"precise\":\"0.3\"},\"precision\":1,\"options\":null,\"param_class\":{\"class_id\":\"slider\",\"label\":\"生成随机性\"}},{\"name\":\"max_tokens\",\"label\":\"最大回复长度\",\"desc\":\"控制模型输出的Tokens 长度上限。通常 100 Tokens 约等于 150 个中文汉字。\",\"type\":\"int\",\"min\":\"1\",\"max\":\"12288\",\"default_val\":{\"default_val\":\"4096\"},\"options\":null,\"param_class\":{\"class_id\":\"slider\",\"label\":\"输入及输出设置\"}}]',
         1, 0, 0, 0, NULL)
     ON DUPLICATE KEY UPDATE
     id = VALUES(id);
