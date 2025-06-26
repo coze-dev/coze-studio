d66efdcb7d04da7886cf7507eb60ec0c7de6e917
zhuangjie.1125 <zhuangjie.1125@bytedance.com>
2025-06-26 21:41:25 +0800

    feat(backend): remove python_script.py use python env to run code

--- scripts/setup/python.sh
+++ scripts/setup/python.sh
@@ -27,7 +27,7 @@ fi
 echo "Installing required Python packages"
 source "$VENV_DIR/bin/activate"
 pip install --upgrade pip
-pip install pillow pdfplumber RestrictedPython python-docx
+pip install pillow pdfplumber python-docx
 
 if [ $? -ne 0 ]; then
     echo "Failed to install Python packages - aborting startup"
@@ -56,14 +56,7 @@ else
     exit 1
 fi
 
-RUN_PYTHON_SCRIPT="$BACKEND_DIR/infra/impl/coderunner/script/python_script.py"
 
-if [ -f "$RUN_PYTHON_SCRIPT" ]; then
-    cp "$RUN_PYTHON_SCRIPT" "$BIN_DIR/python_script.py"
-else
-    echo "❌ RUN_PYTHON_SCRIPT file not found"
-    exit 1
-fi
 
 
 
