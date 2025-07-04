3382fd9ec7284733a5422889c78f1d42f29bdacb
shentong.martin <shentong.martin@bytedance.com>
2025-07-04 10:57:13 +0800

    refactor: remove redundant 'mode' configuration for sub-workflow node
    Change-Id: I2e2d55906cb13b3d96181433366d8abf4537f60d

--- backend/domain/workflow/internal/canvas/adaptor/to_schema.go
+++ backend/domain/workflow/internal/canvas/adaptor/to_schema.go
@@ -989,17 +989,6 @@ func toSubWorkflowNodeSchema(ctx context.Context, n *vo.Node) (*compose.NodeSche
 		SubWorkflowSchema: subWorkflowSC,
 	}
 
-	terminationType := n.Data.Inputs.TerminationType
-
-	// TODO: this may be wrong, termination type and streaming mode are not the same thing
-	if terminationType == 0 {
-		ns.SetConfigKV("Mode", nodes.NonStreaming)
-	} else if terminationType == 1 {
-		ns.SetConfigKV("Mode", nodes.Streaming)
-	} else {
-		return nil, fmt.Errorf("sub workflow node's terminationType is not supported: %d", terminationType)
-	}
-
 	workflowIDStr := n.Data.Inputs.WorkflowID
 	if workflowIDStr == "" {
 		return nil, fmt.Errorf("sub workflow node's workflowID is empty")
@@ -1989,7 +1978,7 @@ func buildClauseFromParams(params []*vo.Param) (*database.Clause, error) {
 
 func parseBatchMode(n *vo.Node) (
 	batchN *vo.Node, // the new batch node
-	enabled bool, // whether the node has enabled batch mode
+	enabled bool,    // whether the node has enabled batch mode
 	err error) {
 	if n.Data == nil || n.Data.Inputs == nil {
 		return nil, false, nil
