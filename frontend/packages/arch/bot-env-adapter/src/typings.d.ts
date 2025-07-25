0a638cd8e6b22a1a218e4e40fed437bbb4d6b45b
gaokekuan <gaokekuan@bytedance.com>
2025-07-25 13:11:31 +0800

    chore: 更新依赖包版本并添加版权声明
    更新 @coze/chat-sdk 和 @coze/api 的版本至最新 beta 和 alpha 版本
    为 pkg-root-webpack-plugin 添加缺失的版权声明
    调整 typings.d.ts 中常量声明的顺序

--- frontend/packages/arch/bot-env-adapter/src/typings.d.ts
+++ frontend/packages/arch/bot-env-adapter/src/typings.d.ts
@@ -1,19 +1,3 @@
-/*
- * Copyright 2025 coze-dev Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /* eslint-disable */
 /* prettier-ignore */
 // 基于src/index.ts自动生成，请勿手动修改
@@ -102,9 +86,9 @@ declare const SOCIAL_SCENE_FRONTIER_SERVICE_KEY: number;
 declare const TERMS_OF_SERVICE: string;
 declare const UPLOAD_CDN: string;
 declare const UPLOAD_CDN_CN: string;
+declare const UPLOAD_CDN_OCI: string;
 declare const UPLOAD_CDN_SG: string;
 declare const UPLOAD_CDN_VA: string;
-declare const UPLOAD_CDN_OCI: string;
 declare const VOLCANO_IDENTITY_DOMAIN: string | null;
 declare const VOLCANO_PLATFORM_APP_KEY: string | null;
 declare const VOLCANO_PLATFORM_ID: number | null;
