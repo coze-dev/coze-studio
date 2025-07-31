63521d023be97051986fe3acb93eacb6269c3363
lipandeng <lipandeng@bytedance.com>
2025-07-31 15:16:31 +0800

    chore: add hint for pip install

--- scripts/setup/change_user_password.sh
+++ scripts/setup/change_user_password.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# Copyright 2025 coze-dev Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 # Script for updating user password
 # Usage: ./change_user_password.sh <email> [password]
@@ -70,7 +86,10 @@ EOF
 # Check if argon2-cffi is installed
 if ! python -c "import argon2" 2>/dev/null; then
     echo "Error: argon2-cffi library is required"
-    echo "Please run: pip install argon2-cffi"
+    echo "If your version of python is python2"
+    echo "    Please run: pip install argon2"
+    echo "If your version of python is python3"
+    echo "    Please run: pip install argon2-cffi"
     exit 1
 fi
 
