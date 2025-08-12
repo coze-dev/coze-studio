87d1dd252ee46a25c0bddbb1b4f4f5cbbb93f95c
zhuangjie.1125 <zhuangjie.1125@bytedance.com>
2025-08-12 22:35:36 +0800

    feat: workflow cross domain support conversation & other domain add related called methods

--- backend/crossdomain/contract/workflow/workflow.go
+++ backend/crossdomain/contract/workflow/workflow.go
@@ -38,19 +38,33 @@ type Workflow interface {
 		allInterruptEvents map[string]*workflowEntity.ToolInterruptEvent) einoCompose.Option
 	ReleaseApplicationWorkflows(ctx context.Context, appID int64, config *ReleaseWorkflowConfig) ([]*vo.ValidateIssue, error)
 	GetWorkflowIDsByAppID(ctx context.Context, appID int64) ([]int64, error)
+
 	SyncExecuteWorkflow(ctx context.Context, config workflowModel.ExecuteConfig, input map[string]any) (*workflowEntity.WorkflowExecution, vo.TerminatePlan, error)
+	StreamExecute(ctx context.Context, config workflowModel.ExecuteConfig, input map[string]any) (*schema.StreamReader[*workflowEntity.Message], error)
 	WithExecuteConfig(cfg workflowModel.ExecuteConfig) einoCompose.Option
 	WithMessagePipe() (compose.Option, *schema.StreamReader[*entity.Message], func())
+	InitApplicationDefaultConversationTemplate(ctx context.Context, spaceID int64, appID int64, userID int64) error
 }
 
 type ExecuteConfig = workflowModel.ExecuteConfig
 type ExecuteMode = workflowModel.ExecuteMode
-type NodeType = entity.NodeType
 
-type WorkflowMessage = entity.Message
+type WorkflowMessage = workflowEntity.Message
+type NodeType = entity.NodeType
+type MessageType = entity.MessageType
+type InterruptEvent = workflowEntity.InterruptEvent
+type EventType = workflowEntity.InterruptEventType
+
+const (
+	Answer       MessageType = "answer"
+	FunctionCall MessageType = "function_call"
+	ToolResponse MessageType = "tool_response"
+)
 
 const (
 	NodeTypeOutputEmitter NodeType = "OutputEmitter"
+	NodeTypeInputReceiver NodeType = "InputReceiver"
+	NodeTypeQuestion      NodeType = "Question"
 )
 
 const (
@@ -60,6 +74,14 @@ const (
 )
 
 type TaskType = workflowModel.TaskType
+
+type SyncPattern = workflowModel.SyncPattern
+
+const (
+	SyncPatternSync   SyncPattern = "sync"
+	SyncPatternAsync  SyncPattern = "async"
+	SyncPatternStream SyncPattern = "stream"
+)
 
 const (
 	TaskTypeForeground TaskType = "foreground"
