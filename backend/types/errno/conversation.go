0d3fbe4545a32704fe588ce25a7c943849af1efc
lijunwen.gigoo <lijunwen.gigoo@bytedance.com>
2025-08-15 17:22:54 +0800

    fix(singleagent): error tips with chatflow not configured & agent mode change

--- backend/types/errno/conversation.go
+++ backend/types/errno/conversation.go
@@ -37,9 +37,17 @@ const (
 	ErrAgentRun = 103200002
 
 	ErrRecordNotFound = 103200003
+
+	ErrAgentRunWorkflowNotFound = 103200004
 )
 
 func init() {
+
+	code.Register(
+		ErrAgentRunWorkflowNotFound,
+		"The chatflow is not configured. Please configure it and try again.",
+		code.WithAffectStability(false),
+	)
 	code.Register(
 		ErrRecordNotFound,
 		"record not found or nothing to update",
