bf402f5e42285582b3cbbb8595b3a9da6e15baeb
zengxiaohui <csu.zengxiaohui@gmail.com>
2025-10-15 21:12:15 +0800

    chore: update workflow idl

--- frontend/packages/arch/idl/src/auto-generated/workflow_api/namespaces/resource.ts
+++ frontend/packages/arch/idl/src/auto-generated/workflow_api/namespaces/resource.ts
@@ -20,47 +20,16 @@
 /* tslint:disable */
 // @ts-nocheck
 
+import * as resource_common from './resource_common';
+
 export type Int64 = string | number;
 
-export enum AttributeValueType {
-  Unknown = 0,
-  String = 1,
-  Boolean = 2,
-  StringList = 11,
-  BooleanList = 12,
-}
-
-export enum ResourceType {
-  Account = 1,
-  Workspace = 2,
-  App = 3,
-  Bot = 4,
-  Plugin = 5,
-  Workflow = 6,
-  Knowledge = 7,
-  PersonalAccessToken = 8,
-  Connector = 9,
-  Card = 10,
-  CardTemplate = 11,
-  Conversation = 12,
-  File = 13,
-  ServicePrincipal = 14,
-  Enterprise = 15,
-  MigrateTask = 16,
-  Prompt = 17,
-  UI = 18,
-  Project = 19,
-}
-
-export interface AttributeValue {
-  Type: AttributeValueType;
-  Value: string;
-}
-
-export interface ResourceIdentifier {
+export interface AuthData {
   /** 资源类型 */
-  Type: ResourceType;
-  /** 资源Id */
-  Id: string;
+  res_type?: resource_common.ResType;
+  /** 节点鉴权信息 */
+  auth_data?: string;
+  /** 资源ID */
+  res_id?: Int64;
 }
 /* eslint-enable */
